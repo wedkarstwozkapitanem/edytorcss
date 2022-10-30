ab1e1f16368a014cac301594c19940e5e0d05038
wedkarstwozkapitanem <110813767+wedkarstwozkapitanem@users.noreply.github.com>
2022-10-30 09:07:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.querySelector('main').contentEditable = "true";
 
 
-var liczbaelementow = 0; //liczba tresci
-var gdzie = "#tresc_strony";
+var liczbaelementow = 2; //liczba tresci;
+
+
+var gdzie = "#tresc_strony1";
 function dodajstyl(opcja, px) {
     document.getElementById(opcja).addEventListener('change', function () {
         if (px == 'p') {
@@ -12,10 +14,21 @@ function dodajstyl(opcja, px) {
         }
         wypiszkod();
     });
+
+    document.getElementById(opcja).addEventListener('keyup', function () {
+        if (px == 'p') {
+            document.querySelector(gdzie).style[opcja] = document.getElementById(opcja).value + 'px';
+        } else {
+            document.querySelector(gdzie).style[opcja] = document.getElementById(opcja).value
+        }
+        wypiszkod();
+    });
+
 }
 
 function wypiszkod() {
-    if (document.getElementById('tresc_strony').innerHTML != '' && typeof (document.getElementById('tresc_strony')) != undefined) {
+    sprawdzkod();
+    if (/*document.getElementById('tresc_strony').innerHTML != '' &&*/ typeof (document.getElementById('tresc_strony')) != undefined) {
         document.getElementById('kod').value = document.querySelector('main').innerHTML;
     } else {
         edytowaniekody();
@@ -23,29 +36,37 @@ function wypiszkod() {
 }
 function edytowaniekody() {
     document.querySelector('main').innerHTML = document.getElementById('kod').value;
+    zmienmenu();
 }
 
-
-dodajstyl('background');
-dodajstyl('color')
-dodajstyl('fontSize', 'p');
-dodajstyl('fontFamily');
-dodajstyl('fontWeight');
-dodajstyl('textAlign');
-dodajstyl('borderRadius', 'p');
-dodajstyl('letterSpacing', 'p');
-dodajstyl('marginTop', 'p');
-dodajstyl('marginLeft', 'p');
-dodajstyl('padding', 'p');
-wypiszkod();
-
+window.onload = function () {
+    dodajstyl('background');
+    dodajstyl('color')
+    dodajstyl('fontSize', 'p');
+    dodajstyl('fontFamily');
+    dodajstyl('fontWeight');
+    dodajstyl('textAlign');
+    dodajstyl('borderRadius', 'p');
+    dodajstyl('letterSpacing', 'p');
+    dodajstyl('marginTop', 'p');
+    dodajstyl('marginLeft', 'p');
+    dodajstyl('padding', 'p');
+    dodajstyl('opacity');
+    dodajstyl('width', 'p');
+    dodajstyl('height', 'p');
+    wypiszkod();
+}
 
 function odtworz() {
     document.querySelector('main').innerHTML = '<div id="tresc_strony" style="background: black;font-size: 104px;color:#ffff00;font-family: cursive;font-weight: 900;text-align: center;">';
 }
 
 function cien(co) {
-    document.querySelector(gdzie).style[co] = `${document.getElementById('x').value}px ${document.getElementById('y').value}px ${document.getElementById('r').value}px ${document.getElementById('c').value}`;
+    if (co == "textShadow") {
+        document.querySelector(gdzie).style[co] = `${document.getElementById('x').value}px ${document.getElementById('y').value}px ${document.getElementById('r').value}px ${document.getElementById('c').value}`;
+    } else if (co == "boxShadow") {
+        document.querySelector(gdzie).style[co] = `${document.getElementById('xe').value}px ${document.getElementById('ye').value}px ${document.getElementById('re').value}px ${document.getElementById('ce').value}`;
+    }
     wypiszkod();
 }
 function border() {
@@ -53,60 +74,125 @@ function border() {
     wypiszkod();
 }
 function nowyelementw() {
-    let nowyelement = document.createElement('div');
-    nowyelement.classList = gdzie + i;
-    document.querySelector(gdzie).appendChild(nowyelement);
+    let nowyelementp = document.createElement('div');
+    nowyelementp.classList = gdzie + i;
+    document.querySelector(gdzie).appendChild(nowyelementp);
     wypiszkod();
     i++;
 }
 
-function nowyelement() {
-    let nowyelement = document.createElement('div');
-    nowyelement.id = 'tresc_strony' + liczbaelementow;
-    nowyelement.innerText = "Tutaj nowa treść";
-    nowyelement.style.position = "relative";
-    nowyelement.style.fontSize = "48px";
-    document.querySelector('main').appendChild(nowyelement);
+function nowyelement(g) {
+    let nowyelementp = document.createElement('div');
+    nowyelementp.id = 'tresc_strony' + liczbaelementow;
+    nowyelementp.innerText = "Tutaj nowa treść";
+    nowyelementp.style.position = "relative";
+    nowyelementp.style.fontSize = "48px";
+    if (g == 'p') {
+        document.querySelector('main').appendChild(nowyelementp);
+    } else {
+        document.querySelector(gdzie).appendChild(nowyelementp);
+    }
     wypiszkod();
     gdzie = '#tresc_strony' + liczbaelementow;
-   
+
 
     for (i = 0; i < document.querySelectorAll('input').length; i++) {
         if (document.querySelectorAll('input')[i].type == "number") {
             document.querySelectorAll('input')[i].value = '0';
-            document.getElementById('fontSize').value = "48";
-            document.getElementById('textAlign').value = "left";
         }
     }
     ktoryelement();
 
-
-   liczbaelementow = parseFloat(liczbaelementow) + parseFloat(1);
+    men();
+    zmienid();
 }
 
+function men() {
+    document.getElementById('fontSize').value = "48";
+    document.getElementById('textAlign').value = "left";
+    document.getElementById('color').value = '#000000';
+    document.getElementById('background').value = '#000000';
+    document.getElementById('fontFamily').value = "auto";
+    document.getElementById('x').value = "0";
+    document.getElementById('y').value = "0";
+    document.getElementById('r').value = "0";
+    document.getElementById('xe').value = "0";
+    document.getElementById('ye').value = "0";
+    document.getElementById('re').value = "0";
+    document.getElementById('border_gruboś').value = "0";
+    liczbaelementow = parseFloat(liczbaelementow) + parseFloat(1);
+}
 
+function usunelement() {
+
+    document.querySelector(gdzie).remove();
+    document.getElementById('kod').value = document.querySelector('main').innerHTML;
+    //  document.querySelector('main').innerHTML = "";
+    //  wypiszkod();
+    zmienid();
+
+    if (document.querySelector('main').innerHTML == "") {
+        nowyelementp = document.createElement('div');
+        nowyelementp.id = "tresc_strony";
+        document.querySelector('main').appendChild(nowyelementp);
+    }
+    sprawdzkod();
+    wypiszkod();
+    men();
+    zmienmenu();
+}
 
 ktoryelement();
 
 function ktoryelement() {
     document.querySelector(gdzie).addEventListener('click', function (p) {
         gdzie = '#' + p.path[0].attributes[0].value;
+        zmienmenu();
     })
 }
 
+function sprawdzkod() {
+    if (document.querySelector('main').innerHTML == '' || document.querySelector('main').innerHTML == '\n            \n        ' || gdzie == "tresc_strony") {
+        nowyelementp = document.createElement('div');
+        nowyelementp.style.position = "relative";
+        nowyelementp.innerText = "Pisz tutaj tekst";
+        //     nowyelement.style.width = "100%";
+        nowyelementp.id = "tresc_strony0";
+        nowyelementp.style.fontSize = "48px";
+        document.querySelector('main').appendChild(nowyelementp);
 
+
+
+
+
+
+        zmienid();
+        wypiszkod();
+    }
+}
 
 
 
 function pobieraniestylowcss(styl) {
-   document.getElementById(styl).value = document.getElementById(gdzie.substr(1)).style[styl];
+    sprawdzkod();
+    let p = document.getElementById(gdzie.substr(1)).style[styl];
+    if (p.indexOf('px') != '-1') {
+        w = p.slice(0, -2);
+        parseFloat(w);
+    } else {
+        w = p;
+    }
+
+
+    document.getElementById(styl).value = w;
+
 }
 
 
 function pobierzaznaczonytekst() {
-   let html =  document.querySelector('main').innerHTML;
-   let zaznaczonytekst = window.getSelection();
-   console.log(zaznaczonytekst);
+    let html = document.querySelector('main').innerHTML;
+    let zaznaczonytekst = window.getSelection();
+    console.log(zaznaczonytekst);
 }
 
 
@@ -124,5 +210,80 @@ function zmienmenu() {
     pobieraniestylowcss('marginTop', 'p');
     pobieraniestylowcss('marginLeft', 'p');
     pobieraniestylowcss('padding', 'p');
+    pobieraniestylowcss('opacity');
+    pobieraniestylowcss('width', 'p');
+    pobieraniestylowcss('height', 'p');
     wypiszkod();
+}
+
+
+
+function uruchomstrone() {
+    document.getElementById('powiekszonastrona').style.display = "block";
+    document.getElementById('strona').innerHTML = document.querySelector('main').innerHTML;
+    document.location.hash = "strona_powiekszona";
+    document.getElementById('powiekszonykod').style.display = "none";
+    document.getElementById('edycja').style.display = "none";
+}
+
+function pokazkod() {
+    document.getElementById('powiekszonastrona').style.display = "none";
+    document.getElementById('powiekszonykod').style.display = "block";
+    document.getElementById('kodpowieksz').value = document.querySelector('main').innerHTML;
+    document.location.hash = "kod_zrodlowy";
+    document.getElementById('edycja').style.display = "none";
+}
+
+
+function wypiszkodpow() {
+    document.querySelector('main').innerHTML = document.getElementById('kodpowieksz').value;
+    document.getElementById('kod').value = document.getElementById('kodpowieksz').value;
+    wypiszkod();
+    zmienmenu();
+}
+
+
+
+function zamknijstrone(co) {
+    document.getElementById(co).style.display = 'none'
+    document.location.hash = "edytuj";
+    document.getElementById('edycja').style.display = "block";
+}
+
+
+
+
+
+
+
+
+function zmienid() {
+    liczbaelementow = document.querySelectorAll('main div').length;
+    for (i = 0; i < liczbaelementow; i++) {
+        let ktory = parseFloat(i) + parseFloat(1);
+        document.querySelectorAll('main div')[i].id = "tresc_strony" + ktory;
+
+
+        document.querySelectorAll('main div')[i].addEventListener('change', function () {
+            if (px == 'p') {
+                document.querySelectorAll('main div')[i].style[opcja] = document.getElementById(opcja).value + 'px';
+            } else {
+                document.querySelectorAll('main div')[i].style[opcja] = document.getElementById(opcja).value
+            }
+            wypiszkod();
+        });
+
+        document.querySelectorAll('main div')[i].addEventListener('keyup', function () {
+            if (px == 'p') {
+                document.querySelector(gdzie).style[opcja] = document.getElementById(opcja).value + 'px';
+            } else {
+                document.querySelector(gdzie).style[opcja] = document.getElementById(opcja).value
+            }
+            wypiszkod();
+        });
+    }
+    liczbaelementow = parseFloat(liczbaelementow);
+    gdzie = "#tresc_strony" + liczbaelementow;
+    wypiszkod();
+    zmienmenu();
 }
